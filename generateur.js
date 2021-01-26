0c1a50141aab6cdb26deb1567d8bdb93083697dc
Antoine Buquet <antoine.buquet49@gmail.com>
2021-01-26 21:38:07 +0100

    Version perso du code de Thomas avec numérotation et clock du generateur

--- generateur.js
+++ generateur.js
@@ -1,9 +1,5 @@
-function Generateur(x,y,vitesse,ctx){
-	
-	ctx.beginPath();
-	ctx.arc(x, y, 15,0, Math.PI * 2, false);
-	ctx.strokeStyle = "blue";
-	ctx.fillStyle = "red";
-	ctx.stroke();
-	console.log("oui");
+function Generateur(x,y,taille,vitesse,ctx,Positions)
+{
+	ctx.fillStyle = 'red';  
+ 	ctx.fillRect(x, y, 10, taille); 
 }
