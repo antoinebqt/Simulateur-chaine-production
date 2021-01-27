0412e8010a4bf86897736a8df48279bc17175564
Xomerian <77843593+Xomerian@users.noreply.github.com>
2021-01-27 10:48:43 +0100

    Update convoyeur.js
    Fusion entre antoine et moi
    Bug

--- convoyeur.js
+++ convoyeur.js
@@ -2,17 +2,15 @@ function Convoyeur(x,y,longueur,vitesse,ctx,Positions,mouvement,taillecube)
 {
   	ctx.fillStyle = 'blue';  
  	ctx.fillRect(x, y, longueur,3);
- 	for (let i = 0; i < max; i++){
  					if (mouvement[i]==0)
  					{
-				 		if ((Positions[i][0]<=(x+longueur-1)) && ((Positions[i][0]+taillecube)>=x)&&(Positions[i][0]!='')){
+				 		if ((Positions[i][0]<=(x+longueur-1)) && ((Positions[i][0]+taillecube)>=x)&&(Positions[i][0]!='') && (Positions[i][1]+taillecube == y - 1)){
 					 		var x = Positions[i][0];
 					 		var y = Positions[i][1];
 					 		Positions[i] = [x + 1,y];
 					 		mouvement[i]=1;
 				 		}
 			 		}
-			 	}
 
 }
 
