5d5d2becf5abd2bbb4483508f876b6dbfc4b6d9f
Xomerian <77843593+Xomerian@users.noreply.github.com>
2021-01-27 10:15:16 +0100

    Update convoyeur.js
    -Fonction deplacement passé dans le convoyeur
    -Arret en bout de ligne
    -Bug de recuperation de la ligne

--- convoyeur.js
+++ convoyeur.js
@@ -1,6 +1,24 @@
-function Convoyeur(x,y,longueur,vitesse,ctx,Positions)
+function Convoyeur(x,y,longueur,vitesse,ctx,Positions,mouvement,taillecube)
 {
   	ctx.fillStyle = 'blue';  
- 	ctx.fillRect(x, y, longueur,3); 
+ 	ctx.fillRect(x, y, longueur,3);
+ 	for (let i = 0; i < max; i++){
+ 					if (mouvement[i]==0)
+ 					{
+ 						mouvement[i]=1;
+				 		if ((Positions[i][0]<=(x+longueur)) && ((Positions[i][0]+taillecube)>=x)&&(Positions[i][0] != '')){
+					 		var x = Positions[i][0];
+					 		var y = Positions[i][1];
+					 		Positions[i] = [x + 1,y];
+				 		}
+			 		}
+			 	}
+
+}
+
+function FirstConvoyeur(x,y,longueur,vitesse,ctx,Positions,mouvement,taillecube)
+{
+  	ctx.fillStyle = 'blue';  
+ 	ctx.fillRect(x, y, longueur,3);
 
 }
