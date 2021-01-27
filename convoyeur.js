04b06f78366398a3de42893ca710387770e67698
Xomerian <77843593+Xomerian@users.noreply.github.com>
2021-01-27 11:17:11 +0100

    Update convoyeur.js
    Bug paire-impair

--- convoyeur.js
+++ convoyeur.js
@@ -3,14 +3,12 @@ function Convoyeur(x,y,longueur,vitesse,ctx,Positions,mouvement,taillecube)
   	ctx.fillStyle = 'blue';  
  	ctx.fillRect(x, y, longueur,3);
  	for (let i = 0; i < max; i++){
- 					if (mouvement[i]==0)
+ 					if((mouvement[i]==0) && (Positions[i][0]<=(x+longueur-1)) && ((Positions[i][0]+taillecube)>=x)&&(Positions[i][0]!='') && ((Positions[i][1]+taillecube)>(y-3))&&((Positions[i][1]+taillecube)<(y-1)) && (Positions[i][1]!=''))
  					{
-				 		if((Positions[i][0]<=(x+longueur-1)) && ((Positions[i][0]+taillecube)>=x)&&(Positions[i][0]!='') && ((Positions[i][1]+taillecube)==(y-2)) && (Positions[i][1]!='')){
 					 		var x = Positions[i][0];
 					 		var y = Positions[i][1];
 					 		Positions[i] = [x + 1,y];
 					 		mouvement[i]=1;
-				 		}
 			 		}
 	}
 
