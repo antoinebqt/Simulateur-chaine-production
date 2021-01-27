4613a9b7bb37cc5e5988bf00d7d29dda30a12505
Xomerian <77843593+Xomerian@users.noreply.github.com>
2021-01-27 11:33:36 +0100

    Update convoyeur.js
    Patch

--- convoyeur.js
+++ convoyeur.js
@@ -3,11 +3,10 @@ function Convoyeur(x,y,longueur,vitesse,ctx,Positions,mouvement,taillecube)
   	ctx.fillStyle = 'blue';  
  	ctx.fillRect(x, y, longueur,3);
  	for (let i = 0; i < max; i++){
- 					if((mouvement[i]==0) && (Positions[i][0]<=(x+longueur-1)) && ((Positions[i][0]+taillecube)>=x)&&(Positions[i][0]!='') && ((Positions[i][1]+taillecube)>(y-3))&&((Positions[i][1]+taillecube)<(y-1)) && (Positions[i][1]!=''))
- 					{
-					 		var x = Positions[i][0];
-					 		var y = Positions[i][1];
-					 		Positions[i] = [x + 1,y];
+ 					if((mouvement[i]==0) && (Positions[i][0]<=(x+longueur-1)) && ((Positions[i][0]+taillecube)>=x)&&(Positions[i][0]!='') && ((Positions[i][1]+taillecube)>(y-3))&&((Positions[i][1]+taillecube)<(y-1)) && (Positions[i][1]!='')){
+					 		var a = Positions[i][0];
+					 		var b = Positions[i][1];
+					 		Positions[i] = [a + 1,b];
 					 		mouvement[i]=1;
 			 		}
 	}
