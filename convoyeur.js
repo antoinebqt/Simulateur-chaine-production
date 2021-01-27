9251588b36e210a2c89e6f4a33b3a2e224392ef7
Xomerian <77843593+Xomerian@users.noreply.github.com>
2021-01-27 10:22:15 +0100

    Update convoyeur.js
    Patch du convoyeur
    -Bug restant : - cube bloque au gene rateur

--- convoyeur.js
+++ convoyeur.js
@@ -5,11 +5,11 @@ function Convoyeur(x,y,longueur,vitesse,ctx,Positions,mouvement,taillecube)
  	for (let i = 0; i < max; i++){
  					if (mouvement[i]==0)
  					{
- 						mouvement[i]=1;
-				 		if ((Positions[i][0]<=(x+longueur)) && ((Positions[i][0]+taillecube)>=x)&&(Positions[i][0] != '')){
+				 		if ((Positions[i][0]<=(x+longueur-1)) && ((Positions[i][0]+taillecube)>=x)&&(Positions[i][0]!='')){
 					 		var x = Positions[i][0];
 					 		var y = Positions[i][1];
 					 		Positions[i] = [x + 1,y];
+					 		mouvement[i]=1;
 				 		}
 			 		}
 			 	}
@@ -20,5 +20,4 @@ function FirstConvoyeur(x,y,longueur,vitesse,ctx,Positions,mouvement,taillecube)
 {
   	ctx.fillStyle = 'blue';  
  	ctx.fillRect(x, y, longueur,3);
-
 }
